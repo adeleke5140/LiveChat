ef208863b218122f8a1de69342df05315bcf3368
adeleke5140 <adelekekehinde06@gmail.com>
2022-10-18 07:21:47 +0100

    add in Profile view component

---
+++ src/views/Profile.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <UpdateProfile />
+    </div>
+</template>
+
+<script setup>
+import UpdateProfile from '../components/UpdateProfile.vue';
+
+</script>
+
+<style scoped>
+
+</style>
