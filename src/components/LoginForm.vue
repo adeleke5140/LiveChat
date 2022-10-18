d078133c14106bdc908bcf000a68ddb3014af198
adeleke5140 <adelekekehinde06@gmail.com>
2022-10-18 15:44:37 +0100

    update the login form to use email instead of username

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,31 +1,34 @@
 <template>
     <form @submit.prevent="handleSubmit">
         <h3>Login</h3>
-        <label for="displayName">
-            Username:
-            <input type="text" id="displayName" placeholder="display name" v-model="displayName" required>
+        <label for="email">
+            Email:
+            <input type="text" id="email" placeholder="email" v-model="email" required>
         </label>
         <label for="password">
             Password:
-            <input type="password" id="password" placeholder="password" v-model="password" required>
+            <input type="password" id="password" placeholder="password" v-model="password" required autocomplete="true">
         </label>
-        <button>Login </button>
+        <Button text="Login" />
     </form>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
 import { auth } from '../firebase/config'
+import Button from './Button.vue';
 
-const displayName = ref('')
+const email = ref('')
 const password = ref('')
 
 const handleSubmit = () => {
-    console.log(displayName.value)
+    console.log(email.value)
 }
 
 onMounted(() => {
-    console.log(auth.currentUser.displayName, auth.currentUser)
+    if (auth.currentUser) {
+        console.log(auth.currentUser.displayName, auth.currentUser)
+    }
 })
 </script>
 
