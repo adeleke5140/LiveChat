5a9482381c09d4a8bd50e57abb073e29561d5787
adeleke5140 <adelekekehinde06@gmail.com>
2022-10-18 07:21:29 +0100

    add useSignup composable

--- src/composables/useSignup.js
+++ src/composables/useSignup.js
@@ -5,24 +5,21 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const error = ref(null);
 const isPending = ref(false);
+// const user = ref("");
 
-const signUp = async (email, password, username) => {
+const signUp = async (email, password) => {
   error.value = null;
   isPending.value = true;
 
   //don't forget error handling
   try {
-    const res = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password,
-      username
-    );
+    const res = await createUserWithEmailAndPassword(auth, email, password);
     if (!res) {
       throw new Error("Could not complete signup!");
     }
     error.value = null;
     isPending.value = false;
+    return res;
   } catch (err) {
     isPending.value = false;
     error.value = err.message;
