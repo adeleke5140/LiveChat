9dea0a2db3e9e9002e60701877e588b17ebc9067
adeleke5140 <adelekekehinde06@gmail.com>
2022-10-18 15:43:32 +0100

    add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+    <button>
+        <span v-if="isPending" class="pending"></span>
+        <span v-else>{{ text }}</span>
+    </button>
+</template>
+
+<script setup>
+const props = defineProps(['isPending', 'text'])
+</script>
+
+<style scoped>
+.pending {
+    display: block;
+    width: 20px;
+    height: 20px;
+    border: 3px solid #eee;
+    border-radius: 50%;
+    border-top-color: #333;
+    animation: spin 1s ease infinite;
+    margin: 0 auto;
+}
+
+@keyframes spin {
+    to {
+        transform: rotate(360deg);
+    }
+}
+</style>
