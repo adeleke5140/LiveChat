627621534bfaf388116e7bf23aeb5ca30f8465b2
adeleke5140 <adelekekehinde06@gmail.com>
2022-10-21 13:20:11 +0100

    add in Chatroom vue

---
+++ src/views/ChatRoom.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Navbar />
+        <h1 class="container heading">Chatroom</h1>
+    </div>
+
+</template>
+
+<script setup>
+import Navbar from '../components/Navbar.vue';
+
+</script>
+
+<style  scoped>
+.heading {
+    padding: .3em .6em;
+    font-weight: bold;
+}
+</style>
