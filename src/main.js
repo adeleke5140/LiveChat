bb19d1b28eed031fdce1185ddfa2757984256599
adeleke5140 <adelekekehinde06@gmail.com>
2022-10-24 00:22:42 +0100

    :ambulance: fix issue with reloading after a user is logged in

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/main.css";
+import { auth } from "./firebase/config";
+import { onAuthStateChanged } from "firebase/auth";
 
-const app = createApp(App)
+let app;
 
-app.use(router)
+onAuthStateChanged(auth, () => {
+  if (!app) {
+    app = createApp(App);
 
-app.mount('#app')
+    app.use(router);
+
+    app.mount("#app");
+  }
+});
