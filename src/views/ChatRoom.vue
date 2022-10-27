b99858bc3f9057f7dfa1c0125796a813d9361b88
adeleke5140 <adelekekehinde06@gmail.com>
2022-10-27 18:53:31 +0100

    update the Chatroom

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -2,12 +2,14 @@
     <div>
         <Navbar />
         <h1 class="container heading">Chatroom</h1>
+        <ChatWindow />
         <NewChatForm />
     </div>
 
 </template>
 
 <script setup>
+import ChatWindow from '../components/ChatWindow.vue';
 import Navbar from '../components/Navbar.vue';
 import NewChatForm from '../components/NewChatForm.vue';
 
