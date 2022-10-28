8f09d0a5e322fa7322a4e9c9047743100cd48034
adeleke5140 <adelekekehinde06@gmail.com>
2022-10-28 19:19:32 +0100

    add the footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+	<footer>
+		Created by <a href="https://github.com/adeleke5140">adeleke5140.</a> Inspired by <a
+			href="https://vienna.earth/meta">Vienna Hypertext</a> and <a
+			href="https://www.youtube.com/c/TheNetNinja">Net Ninja.</a>
+	</footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+footer {
+	position: absolute;
+	bottom: 0;
+	font-size: var(--font-size-smaller);
+}
+
+a {
+	color: var(--link-color);
+	text-decoration: none;
+}
+</style>
